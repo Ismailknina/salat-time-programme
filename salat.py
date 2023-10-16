801e4f06741ad6d639dcded5589c4ada5eb2bd61
Ismail <IsmailKnina01@gmail.com>
2023-10-16 23:29:49 +0100

    adding exceptions to code where sometimes there are bad requests or bad connection and adding a message if the inputs are invalid

--- salat.py
+++ salat.py
@@ -9,13 +9,22 @@ def import_prayer_time(city , country):
         if "data" in info :
             prayers_timing = info["data"]["timings"]
             return prayers_timing
-    except Exception :
-           return "there is a problem here" 
-
+    except requests.exceptions.HTTPError as HTTPError :
+        print("Bad Coonection To server :'HTTPError'")
+    except requests.exceptions.ConnectionError as ConnectionError :
+        print("network problem (DNS failure, refused connection, etc) : 'ConnectionError'")
+    except requests.exceptions.Timeout as Timeout :
+        print("request times out problem :'Timeout' \n Maybe set up for a retry, or continue in a retry loop")
+    except requests.exceptions.TooManyRedirects as TooManyRedirects :
+        print("	request exceeds the configured number of maximum redirections : 'TooManyRedirects'")
+     
 
 city = input("city: ").strip()
 country = input("country: ").strip()
 
-prayers_with_time = import_prayer_time(city , country)
-for Salat_name , time in prayers_with_time.items() :
-    print(f"{Salat_name} : {time}")
+if city and country:
+    prayers_with_time = import_prayer_time(city , country)
+    for Salat_name , time in prayers_with_time.items() :
+        print(f"{Salat_name} : {time}")
+else :
+    print("Unvalid inputs")
